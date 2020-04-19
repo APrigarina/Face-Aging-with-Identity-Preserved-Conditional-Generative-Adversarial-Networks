7796b7d72c94b182ee50bafa1c85f4001cccba50
Prigarina <anna.prigarina@intel.com>
2020-04-19 22:01:10 +0300

    add mobilenet

--- models.py
+++ models.py
@@ -160,16 +160,16 @@ class FaceAging(object):
                                     fused=True):
                     net = slim.convolution2d(x, round(32 * width_multiplier), [3, 3], stride=2, padding='SAME', scope='conv_1')
                     net = slim.batch_norm(net, scope='conv_1/batch_norm')
-                    net = self._depthwise_separable_conv(net, 64, width_multiplier, sc='conv_ds_2')
-                    net = self._depthwise_separable_conv(net, 128, width_multiplier, downsample=True, sc='conv_ds_3')
+                    net = self._depthwise_separable_conv(net, 64, width_multiplier=width_multiplier, sc='conv_ds_2')
+                    net = self._depthwise_separable_conv(net, 128, width_multiplier=width_multiplier, downsample=True, sc='conv_ds_3')
                     self.conv_ds_3 = net
-                    net = self._depthwise_separable_conv(net, 128, width_multiplier, sc='conv_ds_4')
+                    net = self._depthwise_separable_conv(net, 128, width_multiplier=width_multiplier, sc='conv_ds_4')
                     self.conv_ds_4 = net
-                    net = self._depthwise_separable_conv(net, 256, width_multiplier, downsample=True, sc='conv_ds_5')
+                    net = self._depthwise_separable_conv(net, 256, width_multiplier=width_multiplier, downsample=True, sc='conv_ds_5')
                     self.conv_ds_5 = net
-                    net = self._depthwise_separable_conv(net, 256, width_multiplier, sc='conv_ds_6')
+                    net = self._depthwise_separable_conv(net, 256, width_multiplier=width_multiplier, sc='conv_ds_6')
                     self.conv_ds_6 = net
-                    net = self._depthwise_separable_conv(net, 512, width_multiplier, downsample=True, sc='conv_ds_7')
+                    net = self._depthwise_separable_conv(net, 512, width_multiplier=width_multiplier, downsample=True, sc='conv_ds_7')
                     self.conv_ds_7 = net
 
                     net = self._depthwise_separable_conv(net, 512, width_multiplier, sc='conv_ds_8')
