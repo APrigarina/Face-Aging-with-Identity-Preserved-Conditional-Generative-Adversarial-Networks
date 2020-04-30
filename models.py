205775ab657e1e69f82a626dc0d1ecdbe3007e6f
Prigarina <anna.prigarina@intel.com>
2020-04-30 23:32:09 +0300

    remove slim

--- models.py
+++ models.py
@@ -206,19 +206,19 @@ class FaceAging(object):
             # conv1
             net = self.conv2d(x, "conv_1", round(32 * width_multiplier), filter_size=3, strides=2)  # ->[N, 112, 112, 32]
             net = tf.nn.relu(self.bacthnorm(net, "conv_1/bn", is_training=is_training))
-            net = self._depthwise_separable_conv2d(net, 64, self.width_multiplier, "ds_conv_2", is_training=is_training) # ->[N, 112, 112, 64]
-            net = self._depthwise_separable_conv2d(net, 128, self.width_multiplier, "ds_conv_3", downsample=True, is_training=is_training) # ->[N, 56, 56, 128]
-            net = self._depthwise_separable_conv2d(net, 128, self.width_multiplier, "ds_conv_4", is_training=is_training) # ->[N, 56, 56, 128]
-            net = self._depthwise_separable_conv2d(net, 256, self.width_multiplier, "ds_conv_5", downsample=True, is_training=is_training) # ->[N, 28, 28, 256]
-            net = self._depthwise_separable_conv2d(net, 256, self.width_multiplier, "ds_conv_6", is_training=is_training) # ->[N, 28, 28, 256]
-            net = self._depthwise_separable_conv2d(net, 512, self.width_multiplier, "ds_conv_7", downsample=True, is_training=is_training) # ->[N, 14, 14, 512]
-            net = self._depthwise_separable_conv2d(net, 512, self.width_multiplier, "ds_conv_8", is_training=is_training) # ->[N, 14, 14, 512]
-            net = self._depthwise_separable_conv2d(net, 512, self.width_multiplier, "ds_conv_9", is_training=is_training)  # ->[N, 14, 14, 512]
-            net = self._depthwise_separable_conv2d(net, 512, self.width_multiplier, "ds_conv_10", is_training=is_training)  # ->[N, 14, 14, 512]
-            net = self._depthwise_separable_conv2d(net, 512, self.width_multiplier, "ds_conv_11", is_training=is_training)  # ->[N, 14, 14, 512]
-            net = self._depthwise_separable_conv2d(net, 512, self.width_multiplier, "ds_conv_12", is_training=is_training)  # ->[N, 14, 14, 512]
-            net = self._depthwise_separable_conv2d(net, 1024, self.width_multiplier, "ds_conv_13", downsample=True, is_training=is_training) # ->[N, 7, 7, 1024]
-            net = self._depthwise_separable_conv2d(net, 1024, self.width_multiplier, "ds_conv_14", is_training=is_training) # ->[N, 7, 7, 1024]
+            net = self._depthwise_separable_conv2d(net, 64, width_multiplier, "ds_conv_2", is_training=is_training) # ->[N, 112, 112, 64]
+            net = self._depthwise_separable_conv2d(net, 128, width_multiplier, "ds_conv_3", downsample=True, is_training=is_training) # ->[N, 56, 56, 128]
+            net = self._depthwise_separable_conv2d(net, 128, width_multiplier, "ds_conv_4", is_training=is_training) # ->[N, 56, 56, 128]
+            net = self._depthwise_separable_conv2d(net, 256, width_multiplier, "ds_conv_5", downsample=True, is_training=is_training) # ->[N, 28, 28, 256]
+            net = self._depthwise_separable_conv2d(net, 256, width_multiplier, "ds_conv_6", is_training=is_training) # ->[N, 28, 28, 256]
+            net = self._depthwise_separable_conv2d(net, 512, width_multiplier, "ds_conv_7", downsample=True, is_training=is_training) # ->[N, 14, 14, 512]
+            net = self._depthwise_separable_conv2d(net, 512, width_multiplier, "ds_conv_8", is_training=is_training) # ->[N, 14, 14, 512]
+            net = self._depthwise_separable_conv2d(net, 512, width_multiplier, "ds_conv_9", is_training=is_training)  # ->[N, 14, 14, 512]
+            net = self._depthwise_separable_conv2d(net, 512, width_multiplier, "ds_conv_10", is_training=is_training)  # ->[N, 14, 14, 512]
+            net = self._depthwise_separable_conv2d(net, 512, width_multiplier, "ds_conv_11", is_training=is_training)  # ->[N, 14, 14, 512]
+            net = self._depthwise_separable_conv2d(net, 512, width_multiplier, "ds_conv_12", is_training=is_training)  # ->[N, 14, 14, 512]
+            net = self._depthwise_separable_conv2d(net, 1024, width_multiplier, "ds_conv_13", downsample=True, is_training=is_training) # ->[N, 7, 7, 1024]
+            net = self._depthwise_separable_conv2d(net, 1024, width_multiplier, "ds_conv_14", is_training=is_training) # ->[N, 7, 7, 1024]
             self.ds_conv_14 = net
             net = self.avg_pool(net, 7, "avg_pool_15")
             net = tf.squeeze(net, [1, 2], name="SpatialSqueeze")
