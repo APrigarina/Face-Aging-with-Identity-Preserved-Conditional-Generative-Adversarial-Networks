044551d32a184ad83a2e7d05edc58e7d70813160
Prigarina <anna.prigarina@intel.com>
2020-04-30 22:59:16 +0300

    remove slim

--- models.py
+++ models.py
@@ -121,6 +121,83 @@ class FaceAging(object):
                 self.age_logits = fc(age_dropout7, 4096, 5, name='age_fc8', relu=False)
 
             return scope
+
+    # create variable
+    def create_variable(name, shape, initializer,
+        dtype=tf.float32, trainable=True):
+        return tf.get_variable(name, shape=shape, dtype=dtype,
+                initializer=initializer, trainable=trainable)
+
+    # batchnorm layer
+    def bacthnorm(inputs, scope, epsilon=1e-05, momentum=0.99, is_training=True):
+        inputs_shape = inputs.get_shape().as_list()
+        params_shape = inputs_shape[-1:]
+        axis = list(range(len(inputs_shape) - 1))
+
+        with tf.variable_scope(scope):
+            beta = create_variable("beta", params_shape,
+                                initializer=tf.zeros_initializer())
+            gamma = create_variable("gamma", params_shape,
+                                    initializer=tf.ones_initializer())
+            # for inference
+            moving_mean = create_variable("moving_mean", params_shape,
+                                initializer=tf.zeros_initializer(), trainable=False)
+            moving_variance = create_variable("moving_variance", params_shape,
+                                initializer=tf.ones_initializer(), trainable=False)
+        if is_training:
+            mean, variance = tf.nn.moments(inputs, axes=axis)
+            update_move_mean = moving_averages.assign_moving_average(moving_mean,
+                                                    mean, decay=momentum)
+            update_move_variance = moving_averages.assign_moving_average(moving_variance,
+                                                    variance, decay=momentum)
+            tf.add_to_collection(UPDATE_OPS_COLLECTION, update_move_mean)
+            tf.add_to_collection(UPDATE_OPS_COLLECTION, update_move_variance)
+        else:
+            mean, variance = moving_mean, moving_variance
+        return tf.nn.batch_normalization(inputs, mean, variance, beta, gamma, epsilon)
+
+    # depthwise conv2d layer
+    def depthwise_conv2d(inputs, scope, filter_size=3, channel_multiplier=1, strides=1):
+        inputs_shape = inputs.get_shape().as_list()
+        in_channels = inputs_shape[-1]
+        with tf.variable_scope(scope):
+            filter = create_variable("filter", shape=[filter_size, filter_size,
+                                                    in_channels, channel_multiplier],
+                        initializer=tf.truncated_normal_initializer(stddev=0.01))
+
+        return tf.nn.depthwise_conv2d(inputs, filter, strides=[1, strides, strides, 1],
+                                    padding="SAME", rate=[1, 1])
+
+    # conv2d layer
+    def conv2d(inputs, scope, num_filters, filter_size=1, strides=1):
+        inputs_shape = inputs.get_shape().as_list()
+        in_channels = inputs_shape[-1]
+        with tf.variable_scope(scope):
+            filter = create_variable("filter", shape=[filter_size, filter_size,
+                                                    in_channels, num_filters],
+                            initializer=tf.truncated_normal_initializer(stddev=0.01))
+        return tf.nn.conv2d(inputs, filter, strides=[1, strides, strides, 1],
+                            padding="SAME")
+
+    # avg pool layer
+    def avg_pool(inputs, pool_size, scope):
+        with tf.variable_scope(scope):
+            return tf.nn.avg_pool(inputs, [1, pool_size, pool_size, 1],
+                    strides=[1, pool_size, pool_size, 1], padding="VALID")
+
+    # fully connected layer
+    def fc(inputs, n_out, scope, use_bias=True):
+        inputs_shape = inputs.get_shape().as_list()
+        n_in = inputs_shape[-1]
+        with tf.variable_scope(scope):
+            weight = create_variable("weight", shape=[n_in, n_out],
+                        initializer=tf.random_normal_initializer(stddev=0.01))
+            if use_bias:
+                bias = create_variable("bias", shape=[n_out,],
+                                    initializer=tf.zeros_initializer())
+                return tf.nn.xw_plus_b(inputs, weight, bias)
+            return tf.matmul(inputs, weight)
+        
     def _depthwise_separable_conv2d(self, inputs, num_filters, width_multiplier,
                                     scope, downsample=False):
         """depthwise separable convolution 2D function"""
