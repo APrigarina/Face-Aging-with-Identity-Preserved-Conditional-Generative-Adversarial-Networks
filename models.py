83f4935b06b50e4883ddc8b276596ed38265dc68
Prigarina <anna.prigarina@intel.com>
2020-04-30 23:15:28 +0300

    remove slim

--- models.py
+++ models.py
@@ -125,8 +125,7 @@ class FaceAging(object):
     # create variable
     def create_variable(name, shape, initializer,
         dtype=tf.float32, trainable=True):
-        return tf.get_variable(name, shape=shape, dtype=dtype,
-                initializer=initializer, trainable=trainable)
+        return tf.get_variable(name, shape=shape, dtype=dtype, initializer=initializer, trainable=trainable)
 
     # batchnorm layer
     def bacthnorm(inputs, scope, epsilon=1e-05, momentum=0.99, is_training=True):
@@ -135,21 +134,15 @@ class FaceAging(object):
         axis = list(range(len(inputs_shape) - 1))
 
         with tf.variable_scope(scope):
-            beta = self.create_variable("beta", params_shape,
-                                initializer=tf.zeros_initializer())
-            gamma = self.create_variable("gamma", params_shape,
-                                    initializer=tf.ones_initializer())
+            beta = self.create_variable("beta", params_shape, initializer=tf.zeros_initializer())
+            gamma = self.create_variable("gamma", params_shape, initializer=tf.ones_initializer())
             # for inference
-            moving_mean = self.create_variable("moving_mean", params_shape,
-                                initializer=tf.zeros_initializer(), trainable=False)
-            moving_variance = self.create_variable("moving_variance", params_shape,
-                                initializer=tf.ones_initializer(), trainable=False)
+            moving_mean = self.create_variable("moving_mean", params_shape, initializer=tf.zeros_initializer(), trainable=False)
+            moving_variance = self.create_variable("moving_variance", params_shape, initializer=tf.ones_initializer(), trainable=False)
         if is_training:
             mean, variance = tf.nn.moments(inputs, axes=axis)
-            update_move_mean = moving_averages.assign_moving_average(moving_mean,
-                                                    mean, decay=momentum)
-            update_move_variance = moving_averages.assign_moving_average(moving_variance,
-                                                    variance, decay=momentum)
+            update_move_mean = moving_averages.assign_moving_average(moving_mean, mean, decay=momentum)
+            update_move_variance = moving_averages.assign_moving_average(moving_variance, variance, decay=momentum)
             tf.add_to_collection(UPDATE_OPS_COLLECTION, update_move_mean)
             tf.add_to_collection(UPDATE_OPS_COLLECTION, update_move_variance)
         else:
@@ -161,45 +154,35 @@ class FaceAging(object):
         inputs_shape = inputs.get_shape().as_list()
         in_channels = inputs_shape[-1]
         with tf.variable_scope(scope):
-            filter = self.create_variable("filter", shape=[filter_size, filter_size,
-                                                    in_channels, channel_multiplier],
-                        initializer=tf.truncated_normal_initializer(stddev=0.01))
+            filter = self.create_variable("filter", shape=[filter_size, filter_size, in_channels, channel_multiplier], initializer=tf.truncated_normal_initializer(stddev=0.01))
 
-        return tf.nn.depthwise_conv2d(inputs, filter, strides=[1, strides, strides, 1],
-                                    padding="SAME", rate=[1, 1])
+        return tf.nn.depthwise_conv2d(inputs, filter, strides=[1, strides, strides, 1], padding="SAME", rate=[1, 1])
 
     # conv2d layer
     def conv2d(inputs, scope, num_filters, filter_size=1, strides=1):
         inputs_shape = inputs.get_shape().as_list()
         in_channels = inputs_shape[-1]
         with tf.variable_scope(scope):
-            filter = self.create_variable("filter", shape=[filter_size, filter_size,
-                                                    in_channels, num_filters],
-                            initializer=tf.truncated_normal_initializer(stddev=0.01))
-        return tf.nn.conv2d(inputs, filter, strides=[1, strides, strides, 1],
-                            padding="SAME")
+            filter = self.create_variable("filter", shape=[filter_size, filter_size, in_channels, num_filters], initializer=tf.truncated_normal_initializer(stddev=0.01))
+        return tf.nn.conv2d(inputs, filter, strides=[1, strides, strides, 1], padding="SAME")
 
     # avg pool layer
     def avg_pool(inputs, pool_size, scope):
         with tf.variable_scope(scope):
-            return tf.nn.avg_pool(inputs, [1, pool_size, pool_size, 1],
-                    strides=[1, pool_size, pool_size, 1], padding="VALID")
+            return tf.nn.avg_pool(inputs, [1, pool_size, pool_size, 1], strides=[1, pool_size, pool_size, 1], padding="VALID")
 
     # fully connected layer
     def fc(inputs, n_out, scope, use_bias=True):
         inputs_shape = inputs.get_shape().as_list()
         n_in = inputs_shape[-1]
         with tf.variable_scope(scope):
-            weight = self.create_variable("weight", shape=[n_in, n_out],
-                        initializer=tf.random_normal_initializer(stddev=0.01))
+            weight = self.create_variable("weight", shape=[n_in, n_out], initializer=tf.random_normal_initializer(stddev=0.01))
             if use_bias:
-                bias = self.create_variable("bias", shape=[n_out,],
-                                    initializer=tf.zeros_initializer())
+                bias = self.create_variable("bias", shape=[n_out,], initializer=tf.zeros_initializer())
                 return tf.nn.xw_plus_b(inputs, weight, bias)
             return tf.matmul(inputs, weight)
         
-    def _depthwise_separable_conv2d(self, inputs, num_filters, width_multiplier,
-                                    scope, downsample=False):
+    def _depthwise_separable_conv2d(self, inputs, num_filters, width_multiplier, scope, downsample=False):
         """depthwise separable convolution 2D function"""
         num_filters = round(num_filters * width_multiplier)
         strides = 2 if downsample else 1
@@ -218,39 +201,24 @@ class FaceAging(object):
             return tf.nn.relu(bn)
 
 
-    def face_age_mobilenet(self, x, scope_name='mobilenet', if_age=False, reuse=False):
-        width_multiplier=1
+    def face_age_mobilenet(self, x, scope_name='mobilenet', if_age=False, reuse=False, width_multiplier=1):
         with tf.variable_scope(scope_name, reuse=reuse) as sc:
             # conv1
-            net = self.conv2d(x, "conv_1", round(32 * width_multiplier), filter_size=3,
-                         strides=2)  # ->[N, 112, 112, 32]
+            net = self.conv2d(x, "conv_1", round(32 * width_multiplier), filter_size=3, strides=2)  # ->[N, 112, 112, 32]
             net = tf.nn.relu(bacthnorm(net, "conv_1/bn", is_training=self.is_training))
-            net = self._depthwise_separable_conv2d(net, 64, self.width_multiplier,
-                                "ds_conv_2") # ->[N, 112, 112, 64]
-            net = self._depthwise_separable_conv2d(net, 128, self.width_multiplier,
-                                "ds_conv_3", downsample=True) # ->[N, 56, 56, 128]
-            net = self._depthwise_separable_conv2d(net, 128, self.width_multiplier,
-                                "ds_conv_4") # ->[N, 56, 56, 128]
-            net = self._depthwise_separable_conv2d(net, 256, self.width_multiplier,
-                                "ds_conv_5", downsample=True) # ->[N, 28, 28, 256]
-            net = self._depthwise_separable_conv2d(net, 256, self.width_multiplier,
-                                "ds_conv_6") # ->[N, 28, 28, 256]
-            net = self._depthwise_separable_conv2d(net, 512, self.width_multiplier,
-                                "ds_conv_7", downsample=True) # ->[N, 14, 14, 512]
-            net = self._depthwise_separable_conv2d(net, 512, self.width_multiplier,
-                                "ds_conv_8") # ->[N, 14, 14, 512]
-            net = self._depthwise_separable_conv2d(net, 512, self.width_multiplier,
-                                "ds_conv_9")  # ->[N, 14, 14, 512]
-            net = self._depthwise_separable_conv2d(net, 512, self.width_multiplier,
-                                "ds_conv_10")  # ->[N, 14, 14, 512]
-            net = self._depthwise_separable_conv2d(net, 512, self.width_multiplier,
-                                "ds_conv_11")  # ->[N, 14, 14, 512]
-            net = self._depthwise_separable_conv2d(net, 512, self.width_multiplier,
-                                "ds_conv_12")  # ->[N, 14, 14, 512]
-            net = self._depthwise_separable_conv2d(net, 1024, self.width_multiplier,
-                                "ds_conv_13", downsample=True) # ->[N, 7, 7, 1024]
-            net = self._depthwise_separable_conv2d(net, 1024, self.width_multiplier,
-                                "ds_conv_14") # ->[N, 7, 7, 1024]
+            net = self._depthwise_separable_conv2d(net, 64, self.width_multiplier, "ds_conv_2") # ->[N, 112, 112, 64]
+            net = self._depthwise_separable_conv2d(net, 128, self.width_multiplier, "ds_conv_3", downsample=True) # ->[N, 56, 56, 128]
+            net = self._depthwise_separable_conv2d(net, 128, self.width_multiplier, "ds_conv_4") # ->[N, 56, 56, 128]
+            net = self._depthwise_separable_conv2d(net, 256, self.width_multiplier, "ds_conv_5", downsample=True) # ->[N, 28, 28, 256]
+            net = self._depthwise_separable_conv2d(net, 256, self.width_multiplier, "ds_conv_6") # ->[N, 28, 28, 256]
+            net = self._depthwise_separable_conv2d(net, 512, self.width_multiplier, "ds_conv_7", downsample=True) # ->[N, 14, 14, 512]
+            net = self._depthwise_separable_conv2d(net, 512, self.width_multiplier, "ds_conv_8") # ->[N, 14, 14, 512]
+            net = self._depthwise_separable_conv2d(net, 512, self.width_multiplier, "ds_conv_9")  # ->[N, 14, 14, 512]
+            net = self._depthwise_separable_conv2d(net, 512, self.width_multiplier, "ds_conv_10")  # ->[N, 14, 14, 512]
+            net = self._depthwise_separable_conv2d(net, 512, self.width_multiplier, "ds_conv_11")  # ->[N, 14, 14, 512]
+            net = self._depthwise_separable_conv2d(net, 512, self.width_multiplier, "ds_conv_12")  # ->[N, 14, 14, 512]
+            net = self._depthwise_separable_conv2d(net, 1024, self.width_multiplier, "ds_conv_13", downsample=True) # ->[N, 7, 7, 1024]
+            net = self._depthwise_separable_conv2d(net, 1024, self.width_multiplier, "ds_conv_14") # ->[N, 7, 7, 1024]
             self.ds_conv_14 = net
             net = self.avg_pool(net, 7, "avg_pool_15")
             net = tf.squeeze(net, [1, 2], name="SpatialSqueeze")
